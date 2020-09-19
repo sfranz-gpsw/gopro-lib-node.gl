7bdefbb3d4986a7cddc00c4728159087c3f7311c
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-09-18 17:49:49 -0700

    add scripts to build native libs and python env

---
+++ scripts/build_native_env.sh
@@ -0,0 +1,2 @@
+cmake.exe -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake.exe --build cmake-build-debug --config RelWithDebInfo
---
+++ scripts/build_python_env.sh
@@ -0,0 +1,15 @@
+VULKAN_SDK=/mnt/c/VulkanSDK/1.2.131.2
+cmd.exe /C "python.exe -m venv nodegl-env"
+install -d nodegl-env/share
+install -d nodegl-env/share/nodegl
+install -m 644 libnodegl/nodegl.h nodegl-env/Include/
+install -m 644 libnodegl/nodes.specs nodegl-env/share/nodegl/
+install -m 644 cmake-build-debug/libnodegl/RelWithDebInfo/nodegl.lib nodegl-env/Lib/nodegl.lib
+install -m 644 external/win64/pthreads-w32-2-9-1-release/Pre-built.2/lib/x64/pthreadVC2.lib nodegl-env/Lib/
+install -m 644 cmake-build-debug/external/sxplayer-9.5.1/RelWithDebInfo/sxplayer.lib nodegl-env/Lib/
+install -m 644 $VULKAN_SDK/Lib/*.lib nodegl-env/Lib/
+install -m 644 external/win64/ffmpeg-20200831-4a11a6f-win64-dev/lib/*.lib nodegl-env/Lib/
+cmd.exe /C "nodegl-env\\Scripts\\activate.bat && pip.exe install -r pynodegl\requirements.txt"
+cmd.exe /C "nodegl-env\\Scripts\\activate.bat && pip.exe -v install -e pynodegl"
+cmd.exe /C "nodegl-env\\Scripts\\activate.bat && pip.exe install -r pynodegl-utils\requirements.txt"
+cmd.exe /C "nodegl-env\\Scripts\\activate.bat && pip.exe -v install -e pynodegl-utils"
