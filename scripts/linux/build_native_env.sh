06b7a303a13b761450495c135133e8d13ef95795
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-09-21 17:34:18 -0700

    build: use cmake/ninja as default on linux

--- scripts/linux/build_native_env.sh
+++ scripts/linux/build_native_env.sh
@@ -1,3 +1,3 @@
 #set -x
-cmake -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=Debug
-cmake --build cmake-build-debug --config Debug
+cmake -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Ninja" -DGRAPHICS_BACKEND_VULKAN=ON
+cmake --build cmake-build-debug --config Debug -j16
