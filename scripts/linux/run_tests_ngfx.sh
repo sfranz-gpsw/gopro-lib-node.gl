e76562602f74055b6da16cd87daad4144a7491d8
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-10-12 19:07:22 -0700

    script: add test filter

--- scripts/linux/run_tests_ngfx.sh
+++ scripts/linux/run_tests_ngfx.sh
@@ -1,4 +1,5 @@
 rm /tmp/LOG_NGFX
 source nodegl-env/bin/activate
-cd tests && BACKEND=ngfx make -k V=1 -j $NUM_THREADS > /tmp/LOG_NGFX 2>&1; cd -
+TESTS=`cd tests && make tests-list | grep test-$FILTER`
+cd tests && BACKEND=ngfx make -k V=1 -j $NUM_THREADS $TESTS > /tmp/LOG_NGFX 2>&1; cd -
 bash scripts/linux/status.sh /tmp/LOG_NGFX
