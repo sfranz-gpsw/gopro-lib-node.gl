0fcdbc0685c3b91087e71a526f742fe024077336
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-10-19 19:52:25 -0700

    shader_tools: update offline dx12 shader compiler

--- scripts/win64/build_native_env.sh
+++ scripts/win64/build_native_env.sh
@@ -1,2 +1,2 @@
 cmake.exe -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 16 2019" -DGRAPHICS_BACKEND_DIRECT3D12=ON
-cmake.exe --build cmake-build-debug --config RelWithDebInfo
+cmake.exe --build cmake-build-debug --config RelWithDebInfo -j8
--- shader-tools/tools/compile_shaders_dx12.cpp
+++ shader-tools/tools/compile_shaders_dx12.cpp
@@ -6,22 +6,18 @@ using namespace std;
 using namespace ngfx;
 
 int main(int argc, char** argv) {
-    const vector<string> paths = { "ngfx/data/shaders", "nodegl/data/shaders", "nodegl/pynodegl-utils/pynodegl_utils/examples/shaders" };
+    const vector<string> paths = { "ngfx/data/shaders" };
     const vector<string> extensions = {".vert", ".frag", ".comp"};
     auto glslFiles = FileUtil::findFiles(paths, extensions);
     if (argc == 2) glslFiles = FileUtil::filterFiles(glslFiles, argv[1]);
     string outDir = "cmake-build-debug";
-    ShaderTools shaderTools;
+    ShaderTools shaderTools(true);
     string defines = "-DGRAPHICS_BACKEND_DIRECTX12=1";
-    auto spvFiles = shaderTools.compileShaders(glslFiles, defines, outDir, "glsl");
-    auto spvMapFiles = shaderTools.generateShaderMaps(glslFiles, outDir, "glsl");
-    auto hlslFiles = shaderTools.convertShaders(spvFiles, outDir, "hlsl");
-    auto hlslPatchFiles = glob.glob(f"patches/*.hlsl.patch");
-    applyPatches(hlslPatchFiles, outDir);
-    auto hlsllibFiles = shaderTools.compileShaders(hlslFiles, defines, outDir, "hlsl");
-    auto hlslMapFiles = shaderTools.generateShaderMaps(hlslFiles, outDir, "hlsl");
-    auto hlslMapPatchFiles = glob.glob(f"patches/*.hlsl.map.patch");
-    applyPatches(hlslMapPatchFiles, outDir);
+    auto spvFiles = shaderTools.compileShaders(glslFiles, outDir, ShaderTools::FORMAT_GLSL);
+    auto spvMapFiles = shaderTools.generateShaderMaps(glslFiles, outDir, ShaderTools::FORMAT_GLSL);
+    auto hlslFiles = shaderTools.convertShaders(spvFiles, outDir, ShaderTools::FORMAT_HLSL);
+    auto dxcFiles = shaderTools.compileShaders(hlslFiles, outDir, ShaderTools::FORMAT_HLSL);
+    auto hlslMapFiles = shaderTools.generateShaderMaps(dxcFiles, outDir, ShaderTools::FORMAT_HLSL);
     return 0;
 }
 
