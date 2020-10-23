49b1b20ea5eec1c7ac4e9a7ad25226e70fd709a8
Matthieu Bouron <matthieu.bouron@gmail.com>
2020-10-23 15:00:30 +0200

    texture: add and use explicit usage flags

--- libnodegl/backends/gl/gctx_gl.c
+++ libnodegl/backends/gl/gctx_gl.c
@@ -127,7 +127,7 @@ static int offscreen_rendertarget_init(struct gctx *s)
             .format = NGLI_FORMAT_R8G8B8A8_UNORM,
             .width  = config->width,
             .height = config->height,
-            .usage  = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY,
+            .usage  = NGLI_TEXTURE_USAGE_COLOR_ATTACHMENT_BIT,
         };
         s_priv->color = ngli_texture_create(s);
         if (!s_priv->color)
@@ -143,7 +143,7 @@ static int offscreen_rendertarget_init(struct gctx *s)
             .format  = NGLI_FORMAT_R8G8B8A8_UNORM,
             .width   = config->width,
             .height  = config->height,
-            .usage   = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY,
+            .usage   = NGLI_TEXTURE_USAGE_COLOR_ATTACHMENT_BIT,
             .samples = config->samples,
         };
         s_priv->ms_color = ngli_texture_create(s);
@@ -160,7 +160,7 @@ static int offscreen_rendertarget_init(struct gctx *s)
         .width   = config->width,
         .height  = config->height,
         .samples = config->samples,
-        .usage   = NGLI_TEXTURE_USAGE_ATTACHMENT_ONLY,
+        .usage   = NGLI_TEXTURE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
     };
     s_priv->depth = ngli_texture_create(s);
     if (!s_priv->depth)
