af4184ad990a22072630dde24f0e78b2d8cd6fde
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-11-04 23:22:40 -0800

    ngfx: add ngfx source

---
+++ ngfx/src/ngfx/porting/metal/MTLWindow.mm
@@ -0,0 +1,17 @@
+#include "ngfx/porting/metal/MTLWindow.h"
+#include "ngfx/core/DebugUtil.h"
+using namespace ngfx;
+
+bool MTLWindow::shouldClose() {
+    LOG_TRACE("Not supported");
+    return false;
+}
+void MTLWindow::pollEvents() {
+    LOG_TRACE("Not supported");
+}
+
+Window* Window::create(GraphicsContext* graphicsContext, const char* title, std::function<void(Window* thiz)> onWindowCreated,
+                       int w, int h) {
+    LOG_TRACE("Not supported");
+    return nullptr;
+}
