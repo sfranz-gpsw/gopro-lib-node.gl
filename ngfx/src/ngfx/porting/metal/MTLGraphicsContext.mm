6239c5de6934db826990e81be2c3379421b14b41
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-11-04 23:22:40 -0800

    ngfx: add ngfx source

---
+++ ngfx/src/ngfx/porting/metal/MTLGraphicsContext.mm
@@ -0,0 +1,102 @@
+#include "ngfx/porting/metal/MTLGraphicsContext.h"
+#include "ngfx/porting/metal/MTLSurface.h"
+#include "ngfx/core/DebugUtil.h"
+#include <Foundation/Foundation.h>
+using namespace ngfx;
+using namespace std;
+
+void MTLGraphicsContext::create(const char* appName, bool enableDepthStencil, bool debug) {
+    this->enableDepthStencil = enableDepthStencil;
+    this->debug = debug;
+    mtlDevice.create();
+    mtlCommandQueue = [mtlDevice.v newCommandQueue];
+}
+MTLGraphicsContext::~MTLGraphicsContext() {}
+
+void MTLGraphicsContext::setSurface(Surface *surface) {
+    defaultOffscreenSurfaceFormat = PixelFormat(MTLPixelFormatRGBA8Unorm);
+    if (surface && !surface->offscreen) {
+        offscreen = false;
+        mtkView = mtl(surface)->mtkView;
+        mtkView.device = mtlDevice.v;
+        mtlSurfaceFormat = mtkView.colorPixelFormat;
+        surfaceFormat = PixelFormat(mtlSurfaceFormat);
+    } else {
+        offscreen = true;
+        surfaceFormat = defaultOffscreenSurfaceFormat;
+    }
+    if (surface && numSamples != 1) {
+        TODO();
+    }
+    if (surface && enableDepthStencil) {
+        mtlDepthStencilTexture.reset(new MTLDepthStencilTexture);
+        mtlDepthStencilTexture->create(this, surface->w, surface->h);
+        mtkView.depthStencilPixelFormat = mtlDepthStencilTexture->format;
+        depthFormat = PixelFormat(mtlDepthStencilTexture->format);
+        if (numSamples != 1) {
+            TODO();
+        }
+    }
+    mtlDefaultRenderPass = (MTLRenderPass*)getRenderPass({ false, enableDepthStencil, false, numSamples, 1 });
+    mtlDefaultOffscreenRenderPass = (MTLRenderPass*)getRenderPass({true, enableDepthStencil, false, numSamples, 1 });
+    if (surface && !surface->offscreen) createSwapchainFramebuffers(mtkView.drawableSize.width, mtkView.drawableSize.height);
+    createBindings();
+}
+
+RenderPass* MTLGraphicsContext::getRenderPass(RenderPassConfig config) {
+    for (auto& r : mtlRenderPassCache) {
+        if (r->config == config) return &r->mtlRenderPass;
+    }
+    auto renderPassData = make_unique<MTLRenderPassData>();
+    auto result = &renderPassData->mtlRenderPass;
+    mtlRenderPassCache.emplace_back(std::move(renderPassData));
+    return result;
+}
+
+void MTLGraphicsContext::createSwapchainFramebuffers(uint32_t w, uint32_t h) {
+    CAMetalLayer* metalLayer = (CAMetalLayer*)mtkView.layer;
+    numSwapchainImages = metalLayer.maximumDrawableCount;
+    mtlSwapchainFramebuffers.resize(numSwapchainImages);
+    for (auto& fb : mtlSwapchainFramebuffers) {
+        fb.w = mtkView.drawableSize.width;
+        fb.h = mtkView.drawableSize.height;
+    }
+}
+
+void MTLGraphicsContext::createBindings() {
+    device = &mtlDevice;
+    pipelineCache = &mtlPipelineCache;
+    swapchainFramebuffers.resize(numSwapchainImages);
+    for (int j = 0; j<mtlSwapchainFramebuffers.size(); j++)
+        swapchainFramebuffers[j] = &mtlSwapchainFramebuffers[j];
+    defaultRenderPass = mtlDefaultRenderPass;
+    defaultOffscreenRenderPass = mtlDefaultOffscreenRenderPass;
+}
+
+CommandBuffer* MTLGraphicsContext::drawCommandBuffer(int32_t index) {
+    mtlDrawCommandBuffer.v = [mtlCommandQueue commandBuffer];
+    return &mtlDrawCommandBuffer;
+}
+CommandBuffer* MTLGraphicsContext::copyCommandBuffer() {
+    mtlCopyCommandBuffer.v = [mtlCommandQueue commandBuffer];
+    return &mtlCopyCommandBuffer;
+}
+CommandBuffer* MTLGraphicsContext::computeCommandBuffer() {
+    mtlComputeCommandBuffer.v = [mtlCommandQueue commandBuffer];
+    return &mtlComputeCommandBuffer;
+}
+
+void MTLGraphicsContext::submit(CommandBuffer* commandBuffer) {
+    auto mtlCommandBuffer = mtl(commandBuffer);
+    [mtlCommandBuffer->v commit];
+    if (commandBuffer == &mtlComputeCommandBuffer) {
+        [mtlCommandBuffer->v waitUntilCompleted];
+    }
+}
+
+GraphicsContext* GraphicsContext::create(const char* appName, bool enableDepthStencil, bool debug) {
+    LOG("debug: %s", (debug)?"true": "false");
+    auto mtlGraphicsContext = new MTLGraphicsContext();
+    mtlGraphicsContext->create(appName, enableDepthStencil, debug);
+    return mtlGraphicsContext;
+}
---
+++ ngfx/src/ngfx/porting/metal/MTLShaderModule.mm
@@ -0,0 +1,45 @@
+#include "ngfx/porting/metal/MTLShaderModule.h"
+#include "ngfx/porting/metal/MTLDevice.h"
+#include "ngfx/graphics/Config.h"
+#include "ngfx/core/DebugUtil.h"
+#include "ngfx/core/File.h"
+using namespace ngfx;
+using namespace std;
+
+void MTLShaderModule::initFromFile(id<MTLDevice> device, const std::string &filename) {
+    File file;
+#ifdef USE_PRECOMPILED_SHADERS
+    file.read(filename + ".metallib");
+    initFromByteCode(device, file.data.get(), file.size);
+#else
+    ERR("TODO: Support runtime shader compilation");
+#endif
+}
+
+void MTLShaderModule::initFromByteCode(id<MTLDevice> device, void* data, uint32_t size) {
+    NSError* error;
+    dispatch_data_t dispatch_data = dispatch_data_create(data, size, NULL, DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+    mtlLibrary = [device newLibraryWithData:dispatch_data error:&error];
+    NSCAssert(mtlLibrary, @"Failed to load metal library");
+    mtlFunction = [mtlLibrary newFunctionWithName:@"main0"];
+}
+
+template <typename T>
+static std::unique_ptr<T> createShaderModule(Device* device, const std::string& filename) {
+    auto mtlShaderModule = make_unique<T>();
+    mtlShaderModule->initFromFile(mtl(device)->v, filename);
+    mtlShaderModule->initBindings(filename+".metal.map");
+    return mtlShaderModule;
+}
+
+unique_ptr<VertexShaderModule> VertexShaderModule::create(Device* device, const std::string& filename) {
+    return createShaderModule<MTLVertexShaderModule>(device, filename);
+}
+
+unique_ptr<FragmentShaderModule> FragmentShaderModule::create(Device* device, const std::string& filename) {
+    return createShaderModule<MTLFragmentShaderModule>(device, filename);
+}
+
+unique_ptr<ComputeShaderModule> ComputeShaderModule::create(Device* device, const std::string& filename) {
+    return createShaderModule<MTLComputeShaderModule>(device, filename);
+}
