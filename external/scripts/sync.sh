e07c560ba1041e2eec2edba1c14d0babdf34db00
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-11-23 14:16:24 -0800

    build: move prebuilt 3rdparty binaries to github.  TODO: put on public ftp server

--- external/scripts/sync.sh
+++ external/scripts/sync.sh
@@ -6,13 +6,13 @@ TMP_DIR=/tmp/nodegl/external
 if [ $OS == "win64" ]; then
     mkdir -p win64
 
-    wget -nc ftp://localhost/pub/nodegl/external/win64/pkg-config.tgz -P $TMP_DIR/pkg-config
+    wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/pkg-config.tgz?raw=true -O $TMP_DIR/pkg-config/pkg-config.tgz -P $TMP_DIR/pkg-config
     cd win64 && tar xzf $TMP_DIR/pkg-config/pkg-config.tgz; cd -
 
-    wget -nc ftp://localhost/pub/nodegl/external/win64/ffmpeg_x64-windows.tgz -P $TMP_DIR/ffmpeg
+    wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/ffmpeg_x64-windows.tgz?raw=true -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.tgz -P $TMP_DIR/ffmpeg
     cd win64 && tar xzf $TMP_DIR/ffmpeg/ffmpeg_x64-windows.tgz; cd -
 
-    wget -nc ftp://localhost/pub/nodegl/external/win64/pthreads_x64-windows.tgz -P $TMP_DIR/pthreads
+    wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/pthreads_x64-windows.tgz?raw=true -O $TMP_DIR/pthreads/pthreads_x64-windows.tgz -P $TMP_DIR/pthreads
     cd win64 && tar xzf $TMP_DIR/pthreads/pthreads_x64-windows.tgz; cd -
 
     wget -nc https://www.khronos.org/registry/OpenGL/api/GL/glext.h -P $TMP_DIR/gl/include/GL
