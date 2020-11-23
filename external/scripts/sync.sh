9daf38b53c77e7d6344006da1947c4ef1da0c834
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-11-23 11:40:21 -0800

    build: add pthreads dependency

--- external/scripts/sync.sh
+++ external/scripts/sync.sh
@@ -5,11 +5,14 @@ TMP_DIR=/tmp/nodegl/external
 
 if [ $OS == "win64" ]; then
     mkdir -p win64
-    
+
     wget -nc ftp://localhost/pub/nodegl/external/win64/pkg-config.tgz -P $TMP_DIR/pkg-config
     cd win64 && tar xzf $TMP_DIR/pkg-config/pkg-config.tgz; cd -
-    
+
     wget -nc ftp://localhost/pub/nodegl/external/win64/ffmpeg_x64-windows.tgz -P $TMP_DIR/ffmpeg
     cd win64 && tar xzf $TMP_DIR/ffmpeg/ffmpeg_x64-windows.tgz; cd -
-    
+
+    wget -nc ftp://localhost/pub/nodegl/external/win64/pthreads_x64-windows.tgz -P $TMP_DIR/pthreads
+    cd win64 && tar xzf $TMP_DIR/pthreads/pthreads_x64-windows.tgz; cd -
+
 fi
