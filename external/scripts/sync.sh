4a83744e85c88077b6703d95ad9e317dcac09d5e
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-11-30 15:01:43 -0800

    build: update external sync script.  Download prebuilt 3rd-party libs from github action artifact

--- external/scripts/sync.sh
+++ external/scripts/sync.sh
@@ -1,6 +1,7 @@
 OS=$1
+TOKEN=`cat token.cfg`
 echo "OS: [$OS]"
-TMP_DIR=/tmp/nodegl/external
+TMP_DIR=/tmp/gopro-pkg/external
 
 if [ $OS == "win64" ]; then
     mkdir -p win64
@@ -8,11 +9,11 @@ if [ $OS == "win64" ]; then
     mkdir -p $TMP_DIR/pkg-config && wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/pkg-config.tgz?raw=true -O $TMP_DIR/pkg-config/pkg-config.tgz -P $TMP_DIR/pkg-config
     cd win64 && tar xzf $TMP_DIR/pkg-config/pkg-config.tgz; cd -
 
-    mkdir -p $TMP_DIR/ffmpeg && wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/ffmpeg_x64-windows.tgz?raw=true -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.tgz -P $TMP_DIR/ffmpeg
-    cd win64 && tar xzf $TMP_DIR/ffmpeg/ffmpeg_x64-windows.tgz; cd -
+    mkdir -p $TMP_DIR/ffmpeg && wget --header "Authorization: token $TOKEN" -nc https://api.github.com/repos/jmoguillansky-gpsw/gopro-pkg/actions/artifacts/28753508/zip -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.zip -P $TMP_DIR/ffmpeg
+    cd win64 && unzip $TMP_DIR/ffmpeg/ffmpeg_x64-windows.zip -d ffmpeg_x64-windows; cd -
 
-    mkdir $TMP_DIR/pthreads && wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/pthreads_x64-windows.tgz?raw=true -O $TMP_DIR/pthreads/pthreads_x64-windows.tgz -P $TMP_DIR/pthreads
-    cd win64 && tar xzf $TMP_DIR/pthreads/pthreads_x64-windows.tgz; cd -
+    mkdir -p $TMP_DIR/pthreads && wget --header "Authorization: token $TOKEN" -nc https://api.github.com/repos/jmoguillansky-gpsw/gopro-pkg/actions/artifacts/28753514/zip -O $TMP_DIR/pthreads/pthreads_x64-windows.zip -P $TMP_DIR/pthreads
+    cd win64 && unzip $TMP_DIR/pthreads/pthreads_x64-windows.zip -d pthreads_x64-windows; cd -
 
     wget -nc https://www.khronos.org/registry/OpenGL/api/GL/glext.h -P $TMP_DIR/gl/include/GL
     wget -nc https://www.khronos.org/registry/OpenGL/api/GL/glcorearb.h -P $TMP_DIR/gl/include/GL
