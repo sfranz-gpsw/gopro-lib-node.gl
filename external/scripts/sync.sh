e4c9ae6bfb17ddae5b8d949f109ab5d3b6595c7a
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-08 17:19:06 -0800

    external: WIP

--- external/scripts/sync.sh
+++ external/scripts/sync.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-set -xeu
+set -xu
 OS=$1
-TOKEN=`cat ~/.token`
 echo "OS: [$OS]"
 TMP_DIR=/tmp/gopro-pkg/external
 
 if [ $OS == "win64" ]; then
     mkdir -p win64
 
-    mkdir -p $TMP_DIR/pkg-config && wget -nc https://github.com/jmoguillansky-gpsw/gopro-nodegl-thirdparty-binaries/blob/master/external/win64/pkg-config.tgz?raw=true -O $TMP_DIR/pkg-config/pkg-config.tgz -P $TMP_DIR/pkg-config
+    mkdir -p $TMP_DIR/pkg-config && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pkg-config.tgz?raw=true -O $TMP_DIR/pkg-config/pkg-config.tgz
     (cd win64 && tar xzf $TMP_DIR/pkg-config/pkg-config.tgz)
 
-    mkdir -p $TMP_DIR/ffmpeg && wget --header "Authorization: token $TOKEN" -nc https://api.github.com/repos/jmoguillansky-gpsw/gopro-pkg/actions/artifacts/28753508/zip -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.zip -P $TMP_DIR/ffmpeg
+    mkdir -p $TMP_DIR/ffmpeg && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/ffmpeg_x64-windows.zip?raw=true -O $TMP_DIR/ffmpeg/ffmpeg_x64-windows.zip
     (cd win64 && unzip $TMP_DIR/ffmpeg/ffmpeg_x64-windows.zip -d ffmpeg_x64-windows)
 
-    mkdir -p $TMP_DIR/pthreads && wget --header "Authorization: token $TOKEN" -nc https://api.github.com/repos/jmoguillansky-gpsw/gopro-pkg/actions/artifacts/28753514/zip -O $TMP_DIR/pthreads/pthreads_x64-windows.zip -P $TMP_DIR/pthreads
+    mkdir -p $TMP_DIR/pthreads && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/pthreads_x64-windows.zip?raw=true -O $TMP_DIR/pthreads/pthreads_x64-windows.zip
     (cd win64 && unzip $TMP_DIR/pthreads/pthreads_x64-windows.zip -d pthreads_x64-windows)
 
     wget -nc https://www.khronos.org/registry/OpenGL/api/GL/glext.h -P $TMP_DIR/gl/include/GL
@@ -23,4 +22,9 @@ if [ $OS == "win64" ]; then
     wget -nc https://www.khronos.org/registry/EGL/api/KHR/khrplatform.h -P $TMP_DIR/gl/include/KHR
     mkdir -p win64/gl/include && cp -rf $TMP_DIR/gl/include/* win64/gl/include/.
 
+    mkdir -p $TMP_DIR/sdl2 && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/sdl2_x64-windows.zip?raw=true -O $TMP_DIR/sdl2/sdl2_x64-windows.zip
+    (cd win64 && unzip $TMP_DIR/sdl2/sdl2_x64-windows.zip -d sdl2_x64-windows)
+    
+    mkdir -p $TMP_DIR/shaderc && wget -nc https://github.com/jmoguillansky-gpsw/gopro-pkg/blob/master/external/win64/shaderc_x64-windows.tgz?raw=true -O $TMP_DIR/shaderc/shaderc_x64-windows.tgz
+    (cd win64 && tar xzf $TMP_DIR/shaderc/shaderc_x64-windows.tgz)
 fi
