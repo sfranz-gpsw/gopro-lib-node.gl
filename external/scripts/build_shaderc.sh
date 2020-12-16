5e84ca10ebd5522d434059b4efda8ddeb8a39cc0
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-16 12:13:58 -0800

    build: add reference script for shaderc build from source

--- external/scripts/build_shaderc.sh
+++ external/scripts/build_shaderc.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/google/shaderc
 cd shaderc
-cmake.exe -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake.exe -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSHADERC_ENABLE_SHARED_CRT=1
 cmake.exe --build cmake-build-debug -j8 --config RelWithDebInfo
