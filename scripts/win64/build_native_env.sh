2957d926f7de8692a8e43ca58710b21377c85bfe
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-16 14:25:05 -0800

    build: add error checking to build_native_env

--- scripts/win64/build_native_env.sh
+++ scripts/win64/build_native_env.sh
@@ -1,3 +1,4 @@
+set -ex
 cmake.exe -H. -Bcmake-build-debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 16 2019" -DNGFX_GRAPHICS_BACKEND_DIRECT3D12=ON
 cmake.exe --build cmake-build-debug --config RelWithDebInfo -j8
 bash scripts/win64/copy_dlls.sh
