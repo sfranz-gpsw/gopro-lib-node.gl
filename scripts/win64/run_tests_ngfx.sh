cd2cb970b4b23d1535ff1378219d71b86d830bdf
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-16 15:08:05 -0800

    build: set pythonpath on windows

--- scripts/win64/run_tests_ngfx.sh
+++ scripts/win64/run_tests_ngfx.sh
@@ -1,4 +1,4 @@
 rm /tmp/LOG_NGFX
 TESTS=`cd tests && make tests-list | grep test-$FILTER | sed /rtt-feature/d` 
-cd tests && TARGET_OS=Windows BACKEND=ngfx WSLENV=BACKEND/w make -k V=1 -j $NUM_THREADS $TESTS > /tmp/LOG_NGFX 2>&1; cd -
+cd tests && TARGET_OS=Windows BACKEND=ngfx PYTHONPATH=../pynodegl:../pynodegl-utils WSLENV=BACKEND/w:PYTHONPATH/p make -k V=1 -j $NUM_THREADS $TESTS > /tmp/LOG_NGFX 2>&1; cd -
 bash scripts/status.sh /tmp/LOG_NGFX
