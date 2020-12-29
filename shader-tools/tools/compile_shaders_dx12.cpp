3fcca175804f7771658765a2f46b083f37760849
Jeff Moguillansky <jmoguillansky@gopro.com>
2020-12-29 08:33:41 -0800

    shader-tools: update compile_shaders_dx12 tool

--- shader-tools/tools/compile_shaders_dx12.cpp
+++ shader-tools/tools/compile_shaders_dx12.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     const vector<string> extensions = {".vert", ".frag", ".comp"};
     auto glslFiles = FileUtil::findFiles(paths, extensions);
     if (argc == 2) glslFiles = FileUtil::filterFiles(glslFiles, argv[1]);
-    string outDir = "cmake-build-debug/ngfx";
+    string outDir = "ngfx/cmake-build-debug";
     ShaderTools shaderTools(true);
     auto spvFiles = shaderTools.compileShaders(glslFiles, outDir, ShaderTools::FORMAT_GLSL);
     auto spvMapFiles = shaderTools.generateShaderMaps(glslFiles, outDir, ShaderTools::FORMAT_GLSL);
