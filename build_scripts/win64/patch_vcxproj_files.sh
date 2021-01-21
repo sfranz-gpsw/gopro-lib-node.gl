75598fc6a0ab6339b36d36dc8758dce6dbfec781
Jeff Moguillansky <jmoguillansky@gopro.com>
2021-01-21 10:51:49 -0800

    build: (patch_vcxproj_files) add option to enable multiprocessor compilation

--- build_scripts/win64/patch_vcxproj_files.sh
+++ build_scripts/win64/patch_vcxproj_files.sh
@@ -2,4 +2,6 @@
 set -x
 if [ $1 == "--set-runtime-library" ]; then
     find $3 -name '*.vcxproj' | xargs -I {} sed -i -e 's/<RuntimeLibrary>.*<\/RuntimeLibrary>/<RuntimeLibrary>'$2'<\/RuntimeLibrary>/g' {}
+elif [ $1 == "--set-multiprocessor-compilation" ]; then
+    find $3 -name '*.vcxproj' | xargs -I {} sed -i -e 's/[ ]*<\/ClCompile>/<MultiProcessorCompilation>'$2'<\/MultiProcessorCompilation>\n<\/ClCompile>/g' {}
 fi
