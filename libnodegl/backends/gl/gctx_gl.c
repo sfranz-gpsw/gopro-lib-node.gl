f012bb7927a9e61f84a0e4525789fa42c056a10c
Matthieu Bouron <matthieu.bouron@gmail.com>
2021-01-29 10:05:37 +0100

    gctx: add ngli_gctx_wait_idle()

--- libnodegl/backends/gl/gctx_gl.c
+++ libnodegl/backends/gl/gctx_gl.c
@@ -668,6 +668,13 @@ static int gl_query_draw_time(struct gctx *s, int64_t *time)
     return 0;
 }
 
+static void gl_wait_idle(struct gctx *s)
+{
+    struct gctx_gl *s_priv = (struct gctx_gl *)s;
+    struct glcontext *gl = s_priv->glcontext;
+    ngli_glFinish(gl);
+}
+
 static void gl_destroy(struct gctx *s)
 {
     struct gctx_gl *s_priv = (struct gctx_gl *)s;
@@ -806,6 +813,7 @@ const struct gctx_class ngli_gctx_gl = {
     .begin_draw   = gl_begin_draw,
     .end_draw     = gl_end_draw,
     .query_draw_time = gl_query_draw_time,
+    .wait_idle    = gl_wait_idle,
     .destroy      = gl_destroy,
 
     .transform_cull_mode              = gl_transform_cull_mode,
@@ -871,6 +879,7 @@ const struct gctx_class ngli_gctx_gles = {
     .begin_draw   = gl_begin_draw,
     .end_draw     = gl_end_draw,
     .query_draw_time = gl_query_draw_time,
+    .wait_idle    = gl_wait_idle,
     .destroy      = gl_destroy,
 
     .transform_cull_mode              = gl_transform_cull_mode,
