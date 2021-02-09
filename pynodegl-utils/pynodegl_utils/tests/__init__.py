3839cb3619901e2eed03c9d3d15c66761a455700
Jeff Moguillansky <jmoguillansky@gopro.com>
2021-02-09 10:42:17 -0800

    pynodegl-utils: set dll search directories

--- pynodegl-utils/pynodegl_utils/tests/__init__.py
+++ pynodegl-utils/pynodegl_utils/tests/__init__.py
@@ -23,6 +23,16 @@
 import os
 import os.path as op
 import sys
+import platform
+
+if platform.system() == 'Windows':
+    prefix = 'venv'
+    os.add_dll_directory(os.getcwd() + f'/{prefix}/Scripts')
+    env_path = os.getenv("PATH").split(';')
+    for p in env_path:
+        if os.path.exists(p):
+            os.add_dll_directory(p)
+
 
 from pynodegl_utils.com import load_script
 
